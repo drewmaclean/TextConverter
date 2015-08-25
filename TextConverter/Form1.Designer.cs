4a73bcc9ab97de567b07f58d060bfd9188c71110
Drew MacLean <andrew.maclean@umontana.edu>
2015-08-24 23:35:56 -0600

    Initial Commit

---
+++ TextConverter/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TestConverter
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.convertBtn = new System.Windows.Forms.Button();
+      this.convertRtb = new System.Windows.Forms.RichTextBox();
+      this.SuspendLayout();
+      // 
+      // convertBtn
+      // 
+      this.convertBtn.Location = new System.Drawing.Point(230, 467);
+      this.convertBtn.Name = "convertBtn";
+      this.convertBtn.Size = new System.Drawing.Size(75, 23);
+      this.convertBtn.TabIndex = 1;
+      this.convertBtn.Text = "&Convert";
+      this.convertBtn.UseVisualStyleBackColor = true;
+      this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+      // 
+      // convertRtb
+      // 
+      this.convertRtb.Location = new System.Drawing.Point(12, 12);
+      this.convertRtb.Name = "convertRtb";
+      this.convertRtb.Size = new System.Drawing.Size(522, 449);
+      this.convertRtb.TabIndex = 0;
+      this.convertRtb.Text = "";
+      // 
+      // Form1
+      // 
+      this.AllowDrop = true;
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+      this.ClientSize = new System.Drawing.Size(546, 502);
+      this.Controls.Add(this.convertRtb);
+      this.Controls.Add(this.convertBtn);
+      this.Name = "Form1";
+      this.Text = "Text Converter";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button convertBtn;
+    private System.Windows.Forms.RichTextBox convertRtb;
+  }
+}
+
---
+++ TextConverter/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestConverter
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void convertBtn_Click(object sender, EventArgs e)
+    {
+      var sb = new StringBuilder();
+      if (convertRtb.Text.Contains('"'))
+      {
+        foreach (var line in convertRtb.Text.Split('+'))
+        {
+          var l = line.Trim(new char[] {'"', '+', ' '});
+          var k = l.Replace("\"", "");
+          sb.Append(k);
+        }
+        convertRtb.Text = sb.ToString();
+      }
+      else
+      {
+        int i = 0;
+        var s = convertRtb.Text.Split(new char[] {'\r', '\n'});
+        foreach (var line in s)
+        {
+          i++;
+          sb.Append('"');
+          sb.Append(line);
+          sb.Append(" \"");
+          if (i < s.Count())
+          {
+            sb.Append(" +");
+            sb.AppendLine();
+          }
+         
+        }
+        convertRtb.Text = sb.ToString();
+      }
+    }
+  }
+}
